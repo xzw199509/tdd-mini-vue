5ea9a98d8aba6e996d883d73bb19c367cfc5e5f2
xiao1995 <1050149954@qq.com>
2024-01-26 23:32:57 +0800

    响应式系统-抽离track与trigger

--- core/effect.js
+++ core/effect.js
@@ -5,31 +5,37 @@ let activeEffect = null
 export function proxyData(data) {
   return new Proxy(data, {
     get(target, key) {
-      if (!activeEffect) return
-
-      let depsMap = bucket.get(target)
-
-      if (!depsMap) {
-        bucket.set(target, (depsMap = new Map()))
-      }
-      let deps = depsMap.get(key)
-      if (!deps) {
-        depsMap.set(key, (deps = new Set())) // key 就不能重复
-      }
-      // 最后将激活的副作用函数添加到桶中
-      deps.add(activeEffect)
+      track(target, key)
       return target[key]
     },
     set(target, key, newVal) {
       target[key] = newVal
-      const depsMap = bucket.get(target)
-      if (!depsMap) return true // return true 用于set调用必须返回布尔值
-      const effects = depsMap.get(key)
-      effects && effects.forEach((fn) => fn())
+      trigger(target, key)
       return true // return true 用于set调用必须返回布尔值
-      
     },
   })
+}
+
+function track(target, key) {
+  if (!activeEffect) return
+
+  let depsMap = bucket.get(target)
+
+  if (!depsMap) {
+    bucket.set(target, (depsMap = new Map()))
+  }
+  let deps = depsMap.get(key)
+  if (!deps) {
+    depsMap.set(key, (deps = new Set())) // key 就不能重复
+  }
+  // 最后将激活的副作用函数添加到桶中
+  deps.add(activeEffect)
+}
+function trigger(target, key) {
+  const depsMap = bucket.get(target)
+  if (!depsMap) return true // return true 用于set调用必须返回布尔值
+  const effects = depsMap.get(key)
+  effects && effects.forEach((fn) => fn())
 }
 
 export function effect(fn) {
